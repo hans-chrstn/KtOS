2f1402ba394c05f92244498d53a5f3e09585f291
hayato-oo <xuhiko13@gmail.com>
2024-10-11 11:53:43 -0400

    initial commit

---
+++ flake.nix
@@ -0,0 +1,75 @@
+{
+  description = "Making a distro";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    nixpkgs,
+    self,
+  }: let
+    systems = [
+      "aarch64-linux"
+      "i686-linux"
+      "x86_64-linux"
+      "aarch64-darwin"
+      "x86_64-darwin"
+    ];
+    forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f {
+      pkgs = import nixpkgs { inherit system; config = { allowUnfree = true; }; };
+    });
+  in {
+
+    devShells = forAllSystems ({ pkgs }: let 
+      libraries = with pkgs; [
+          ncurses
+          bzip2
+          git
+          gnumake
+          flex
+          bison
+          bc
+          cpio
+          elfutils.dev
+          openssl.dev
+          syslinux
+          dosfstools
+          bash
+          binutils
+          gdb
+          lld
+          libllvm
+          pkg-config
+          coreutils
+          diffutils
+          busybox
+          file
+          findutils
+          gawk
+          gnugrep
+          gzip
+          m4
+          man-db
+          procps
+          psmisc
+          sedutil
+          shadow
+          gnutar
+          util-linux
+          zlib
+          perl
+          python312Full
+          kotlin
+          gcc
+          linuxHeaders
+        ];
+    in {
+      default = pkgs.mkShell {
+          buildInputs = libraries;
+          nativeBuildInputs = libraries;
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath libraries;
+        };
+    });
+  };
+}
