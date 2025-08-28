8ce7179bdb7498ebc405a418fbba7ad661aa5332
hayato-oo <xuhiko13@gmail.com>
2025-08-28 00:43:16 -0400

    #1
    added static binaries
    added lazy commands

--- README.md
+++ README.md
@@ -1,16 +1,1 @@
-generate rootfs:
-    rootfs/
-find . -print0 | cpio --null -ov --format=newc | gzip -9 > ../initramfs.cpio.gz
-
-generate iso:
-grub-mkrescue -o myiso.iso iso/
-
-fix bash tty problem:
-    rootfs/dev
-mknod console c 5 1
-mknod tty0 c 4 0
-mknod null c 1 3
-chmod 600 console
-chmod 666 null
-
 credits to ryanwoodsmall for the static binaries (nixos has been a pain in getting any static binaries)
--- flake.nix
+++ flake.nix
@@ -68,14 +68,44 @@
         ];
     in {
       default = pkgs.mkShell {
-          buildInputs = libraries;
+          buildInputs = libraries ++ [
+            (pkgs.writeShellScriptBin "linux-fetch" ''
+              if [ ! -d ./linux ]; then
+                echo "Cloning Linux kernel..."
+                git clone https://github.com/torvalds/linux.git
+              fi
+            '')
+            (pkgs.writeShellScriptBin "build-iso" ''
+              set -euo pipefail
+              ROOTFS="./rootfs"
+              ISO_DIR="./iso"
+              ISO_OUTPUT="./output/myos.iso"
+              INITRAMFS="$ISO_DIR/boot/initramfs.cpio.gz"
+              KERNEL="$ISO_DIR/boot/bzImage"
+
+              echo "Building initramfs: $ROOTFS -> $INITRAMFS..."
+              cd "$ROOTFS"
+              find . -print0 | cpio --null -ov --format=newc | gzip -9 > ../iso/boot/initramfs.cpio.gz
+              cd ../
+              if [ ! -f "$KERNEL" ]; then
+                echo "bzImage (Kernel) not found.."
+                echo "Please run a linux-fetch and build the kernel!"
+                exit 1
+              fi
+
+              echo "Building ISO: $ISO_OUTPUT"
+              grub-mkrescue -o $ISO_OUTPUT $ISO_DIR
+
+              echo "Done!"
+
+            '')
+          ];
           nativeBuildInputs = libraries;
           LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath libraries;
           shellHook = ''
-            if [ ! -d ./linux ]; then
-              echo "Cloning linux kernel.."
-              git clone https://github.com/torvalds/linux.git
-            fi
+            echo "Run 'linux-fetch' to fetch latest linux kernel"
+            echo "Run 'build-iso' to build the initramfs and iso"
+
           '';
         };
     });
